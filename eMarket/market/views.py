a1702e83e27f13ddbf8c98cdfd1692716e131e46
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-06-04 18:25:43 +0300

    define view to display product details

--- eMarket/market/views.py
+++ eMarket/market/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 
-def product_list(request, category_slug=None):
+def product_list(request, category_identifier=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    if category_slug:
-        category = get_object_or_404(Category, identifier=category_slug)
+    if category_identifier:
+        category = get_object_or_404(Category, identifier=category_identifier)
         products = products.filter(category=category)
     return render(request, 'market/product/list.html', {'category': category, 'categories': categories, 'products': products})
+
+
+def product_detail(request, id, identifier):
+    product = get_object_or_404(Product, id=id, identifier=identifier, available=True)
+    return render(request, 'market/product/detail.html', {'product': product})
