19d72c4d81d6b5a18849d41ab390995253da9cae
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-06-04 18:11:29 +0300

    define view to display all products

--- eMarket/market/views.py
+++ eMarket/market/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, identifier=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'market/product/list.html', {'category': category, 'categories': categories, 'products': products})
