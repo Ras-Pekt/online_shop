bd00ff3ebf89d95d409b675f8a801fae5bea1e91
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-06-12 14:14:48 +0300

    create view for removing and displaying items in cart

--- eMarket/cart/views.py
+++ eMarket/cart/views.py
@@ -7,6 +7,15 @@ from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
+    """
+    Add a product to the cart or update its quantity.
+
+    Args:
+        request (HttpRequest): The request object.
+        product_id (int): The product ID.
+    Returns:
+        HttpResponseRedirect: The cart detail page.
+    """
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
@@ -16,3 +25,44 @@ def cart_add(request, product_id):
         cart.add(product=product, quantity=cleaned_data["quantity"])
 
     return redirect("cart:cart_detail")
+
+
+@require_POST
+def cart_remove(request, product_id):
+    """
+    Remove a product from the cart.
+
+    Args:
+        request (HttpRequest): The request object.
+        product_id (int): The product ID.
+    Returns:
+        HttpResponseRedirect: The cart detail page.
+    """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+
+    return redirect("cart:cart_detail")
+
+
+def cart_detail(request):
+    """
+    Display all items in the cart.
+
+    Args:
+        request (HttpRequest): The request object.
+    Returns:
+        HttpResponse: The response object.
+    """
+
+    # cart = Cart(request)
+    # return render(request, 'cart/detail.html', {'cart': cart})
+
+    cart = Cart(request)
+
+    for item in cart:
+        item["update_quantity_form"] = CartAddProductForm(
+            initial={"quantity": item["quantity"]}
+        )
+
+    return render(request, "cart/detail.html", {"cart": cart})
