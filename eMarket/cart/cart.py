01eb67be753d60bc4475f812daca635826114929
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-06-12 13:55:01 +0300

    create form for adding products to cart

--- eMarket/cart/cart.py
+++ eMarket/cart/cart.py
@@ -22,7 +22,8 @@ class Cart:
 
         self.cart = cart
 
-    def add(self, product, quantity=1, update_quantity=False):
+    # def add(self, product, quantity=1, update_quantity=False):
+    def add(self, product, quantity=1):
         """Add a product to the cart or update its quantity.
 
         Args:
@@ -35,11 +36,16 @@ class Cart:
 
         product_id = str(product.id)
 
-        if product_id not in self.cart:
-            self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
+        # if product_id not in self.cart:
+        #     self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
 
-        if update_quantity:
-            self.cart[product_id]["quantity"] = quantity
+        # if update_quantity:
+        #     self.cart[product_id]["quantity"] = quantity
+        # else:
+        #     self.cart[product_id]["quantity"] += quantity
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {"quantity": quantity, "price": str(product.price)}
         else:
             self.cart[product_id]["quantity"] += quantity
 
@@ -87,7 +93,7 @@ class Cart:
 
     def __len__(self):
         """
-        Count all items in the cart.
+        Custom len() method to count all items in the cart.
 
         Returns:
             int: The total number of items in the cart.
