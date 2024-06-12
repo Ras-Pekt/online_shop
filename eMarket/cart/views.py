be17dab5b8cbd04ffe5e4fae19d014fe8fa19ccd
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-06-12 13:55:48 +0300

    create view for adding items to cart

---
+++ eMarket/cart/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from market.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        cart.add(product=product, quantity=cleaned_data["quantity"])
+
+    return redirect("cart:cart_detail")
