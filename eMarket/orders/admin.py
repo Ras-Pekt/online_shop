9a5731ab84b46d07fe48cc43f3d43dddc183d77c
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-06-14 16:28:00 +0300

    include order models in the admin site

--- eMarket/orders/admin.py
+++ eMarket/orders/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    """
+    Inline class for the OrderItem model to be displayed within the Order model in the Django admin interface.
+    """
+
+    model = OrderItem
+    raw_id_fields = ["product"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Order model to customize the display in the Django admin interface.
+    """
+
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "address",
+        "postal_code",
+        "city",
+        "paid",
+        "created",
+        "updated",
+    ]
+    list_filter = ["paid", "created", "updated"]
+    inlines = [OrderItemInline]
